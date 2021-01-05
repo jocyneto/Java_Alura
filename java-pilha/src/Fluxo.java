91bdc6f430942416c3d4e55600c0b953abb363e8
jocyneto <jocygneto@gmail.com>
2021-01-05 19:21:14 -0300

    Update Fluxo.java

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,22 +1,32 @@
 public class Fluxo {
 
-    public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
-
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
-
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		try {
+			metodo1();	
+		}catch(NullPointerException | ArithmeticException ex) {
+        	System.out.println("E2");
         }
-        System.out.println("Fim do metodo2");        
-    }
+		
+		System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		int z = 10;
+
+		for(int i = 5; i >=0; i--) {
+			System.out.println(z/i);
+		}
+
+
+
+		System.out.println("Fim do metodo2");        
+	}
 }
