b9bfae4429004ebafee07fecce24fbd721f5bdc6
jocyneto <jocygneto@gmail.com>
2021-01-12 17:55:01 -0300

    pilha java

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -4,29 +4,43 @@ public class Fluxo {
 		System.out.println("Ini do main");
 		try {
 			metodo1();	
-		}catch(NullPointerException | ArithmeticException ex) {
-        	System.out.println("E2");
+		}catch(NullPointerException | ArithmeticException | MinhaExcecao ex) {
+        	System.out.println("deu ruim");
+        	ex.printStackTrace();
         }
 		
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao{
+		System.out.println("Ini do metodo1");		
+		metodo2();		
+		System.out.println("Fim do metodo1");
+	}/*OU
+	
+	*private static void metodo1() throws MinhaExcecao{
 		System.out.println("Ini do metodo1");
-		metodo2();
+		try {
+			metodo2();
+		} catch (MinhaExcecao e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Fim do metodo1");
 	}
+	*/
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao{
 		System.out.println("Ini do metodo2");
-		int z = 10;
-
+		/*int z = 10;		
+		
 		for(int i = 5; i >=0; i--) {
 			System.out.println(z/i);
-		}
+		}*/
+		metodo2();
 
+		throw new MinhaExcecao("Deu ruim");
 
-
-		System.out.println("Fim do metodo2");        
+		//System.out.println("Fim do metodo2");        
 	}
 }
