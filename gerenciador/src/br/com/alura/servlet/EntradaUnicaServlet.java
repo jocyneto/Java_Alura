0e80dc12cb6291f3dafdafb4f24a422721b4c268
jocyneto <jocygneto@gmail.com>
2021-01-15 21:09:48 -0300

    login unico

--- gerenciador/src/br/com/alura/servlet/EntradaUnicaServlet.java
+++ gerenciador/src/br/com/alura/servlet/EntradaUnicaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.acao.Acao;
 import br.com.alura.acao.AlteraEmpresa;
@@ -22,8 +23,17 @@ public class EntradaUnicaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		String paramAcao = request.getParameter("acao");
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = sessao.getAttribute("usuarioLogado") == null;
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		
+		if(usuarioNaoEstaLogado && ehUmaAcaoProtegida) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}		
+		
 		String nomeRequisicao = null;
 		
 		String nomeClasse = "br.com.alura.acao." + paramAcao;		
